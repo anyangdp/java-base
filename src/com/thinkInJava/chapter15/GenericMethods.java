dd15a69cd62c549838804890655c5a71028354a0
anyangdp@aliyun <anyangdp@aliyun>
2019-04-09 14:33:52 +0800

    第十五章：范型（进阶读物：《Generics in this java Programming Language》）

---
+++ src/com/thinkInJava/chapter15/GenericMethods.java
@@ -0,0 +1,22 @@
+package com.thinkInJava.chapter15;
+
+/**
+ * @Author anyang
+ * @CreateTime 2019/4/9
+ * @Des
+ */
+public class GenericMethods {
+    public <T> void f(T x) {
+        System.out.println(x.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+        GenericMethods gm = new GenericMethods();
+        gm.f("");
+        gm.f(1);
+        gm.f(1.0);
+        gm.f(1.0F);
+        gm.f('c');
+        gm.f(gm);
+    }
+}
